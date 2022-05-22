fded2fa60466bdf5748835edafe40c5c8448f195
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-05-22 11:52:50 +0700

    change some font

--- src/components/profile/ProfileIntro.tsx
+++ src/components/profile/ProfileIntro.tsx
@@ -4,11 +4,11 @@ export class ProfileIntro extends React.Component {
     render(): React.ReactNode {
         return (
             <div className="mx-auto text-center p-3 my-3">
-                <h3 className="text-4xl text-bold">
+                <h3 className="text-5xl text-bold profile-name">
                     UnknownRori
                 </h3>
-                <p className="m-2">
-                    Hi There! I am a junior fullstack web developer
+                <p className="m-2 text-lg text-bold">
+                    Hello! I am a Fullstack Developer
                 </p>
             </div>
         );
