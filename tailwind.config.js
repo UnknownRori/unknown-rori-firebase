3ec23f213b33a8602c06ae4172dae04c1986dbfd
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-05-22 14:45:37 +0700

    add `fadeIn` Animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,21 @@ module.exports = {
                 'lg': '800px',
                 'xl': '1024px',
                 '2xl': '1280px',
+            },
+            keyframes: {
+                fadeIn: {
+                    '0%': {
+                        opacity: '0%',
+                        transform: 'translateX(-48px)'
+                    },
+                    '100': {
+                        opacity: '100%',
+                        transform: 'translateX(0px)'
+                    }
+                }
+            },
+            animation: {
+                fadeIn: 'fadeIn 1s ease-in-out'
             }
         },
     },
