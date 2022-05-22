559b9b6b0aa2f853e1bc44019643d54028077071
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-05-22 12:02:47 +0700

    fix unused variable

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -4,7 +4,6 @@ import arrow from './../../asset/image/white-arrow.webp';
 
 export class Sidebar extends React.Component{
     render(): React.ReactNode {
-        const text = '=>';
         return (
             <div className="top-44 fixed">
                 <button className="button-secondary fixed m-3 -translate-x-4 w-12 transition-all duration-1000 group peer focus:translate-x-14 z-10">
