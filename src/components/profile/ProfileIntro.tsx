8241a5ec0d7c36202404c6ec23d67b0be80a6b13
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-05-22 15:18:19 +0700

    add typing effect and implement it

--- src/components/profile/ProfileIntro.tsx
+++ src/components/profile/ProfileIntro.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { FadeInText } from "../animation/FadeInText";
+import ReactTypingEffect from 'react-typing-effect';
 
 export class ProfileIntro extends React.Component {
     render(): React.ReactNode {
         return (
-            <div className="mx-auto text-center p-3 my-3">
-                <FadeInText text="UnknownRori" className="text-5xl text-bold profile-name" />
-                <FadeInText text="Hello! I am a Fullstack Developer" className="m-2 text-lg text-bold" />
+            <div className="mx-auto text-center p-3 my-3 flex flex-col">
+                <ReactTypingEffect text={['UnknownRori', 'I am a Fullstack Developer']}
+                    className="text-4xl text-bold"
+                    displayTextRenderer={(text:string, i:number) => {
+                        return (
+                            <p className={i == 0 ? 'profile-name' : ''}>
+                                {text}
+                            </p>
+                        );
+                    }} typingDelay={500} />
             </div>
         );
     }
