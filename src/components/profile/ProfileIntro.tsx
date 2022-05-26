870e87eb252579ef3155d220a153d1268e492804
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-05-26 15:28:34 +0700

    change the font size

--- src/components/profile/ProfileIntro.tsx
+++ src/components/profile/ProfileIntro.tsx
@@ -5,8 +5,10 @@ export class ProfileIntro extends React.Component {
     render(): React.ReactNode {
         return (
             <div className="mx-auto text-center p-3 my-3 flex flex-col">
-                <ReactTypingEffect text={['UnknownRori', 'I am a Fullstack Developer']}
-                    className="text-4xl text-bold"
+                <ReactTypingEffect text={
+                    ['UnknownRori', 'I am a Fullstack Developer.']
+                    }
+                    className="sm:text-2xl md:text-3xl lg:text-4xl text-bold"
                     displayTextRenderer={(text:string, i:number) => {
                         return (
                             <p className={i == 0 ? 'profile-name' : ''}>
