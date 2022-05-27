251023e1137d3725dd502ae1d7d130e4ff8990d6
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-05-28 05:48:26 +0700

    removing default rounded css class

--- src/components/social/Icon.tsx
+++ src/components/social/Icon.tsx
@@ -5,7 +5,7 @@ export class Icon extends React.Component<IconProps> {
     render(): React.ReactNode {
         return (
             <a target='_blank' href={this.props.href} className='m-2'>
-                <img className={"rounded-lg w-12 duration-1000 transition-all " + this.props.className}
+                <img className={"duration-1000 transition-all " + this.props.className}
                     src={this.props.img} alt={this.props.alt} />
             </a>
         );
