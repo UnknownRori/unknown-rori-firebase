88c010a24716849620ddabeeba90fc748ee07f0c
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-05-27 14:58:45 +0700

    implement custom scrollbar styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './asset/css/index.css';
+import './asset/css/scroll.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
