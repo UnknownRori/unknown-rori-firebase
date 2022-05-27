a28c832696009527404b77cf895371a8a298b1e0
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-05-27 14:59:03 +0700

    fill out proper data

--- src/components/projects/ProjectItem.tsx
+++ src/components/projects/ProjectItem.tsx
@@ -4,11 +4,11 @@ import { projectItem } from "../../interface/ProjectItem";
 export class ProjectItem extends React.Component<{ project: projectItem}> {
     render(): ReactNode {
         return(
-            <div className="z-50 md:w-3/12 sm:w-6:12 m-5 p-5 bg-gray-700 bg-opacity-60 rounded-md shadow-2xl ring-1 ring-gray-400 duration-1000
-                           hover:-translate-y-5 hover:bg-gray-800 hover:bg-opacity-95 hover:ring-gray-600 hover:shadow-md"
+            <div className="md:w-3/12 sm:w-6:12 m-5 p-5 bg-gray-700 bg-opacity-60 rounded-md shadow-2xl ring-1 ring-gray-400 duration-1000
+                           hover:z-50 hover:-translate-y-5 hover:bg-gray-800 hover:bg-opacity-95 hover:ring-gray-600 hover:shadow-md"
             >
                 <a href={this.props.project.link} target="_blank">
-                    <img className="" src={this.props.project.image} alt={this.props.project.title} />
+                    <img className="ring-1 ring-gray-700 rounded" src={this.props.project.image} alt={this.props.project.title} />
                     <h2 className="m-2 text-center md:text-xl sm:text-sm">
                         { this.props.project.title }
                     </h2>
--- src/components/projects/ProjectsList.tsx
+++ src/components/projects/ProjectsList.tsx
@@ -2,16 +2,22 @@ import React from "react";
 import { projectItem } from "../../interface/ProjectItem";
 import { ProjectItem } from "./ProjectItem";
 import UnknownRoriPHP from "../../asset/image/projects/php-framework.webp";
+import TicTacToe from "../../asset/image/projects/basic-hotseat-tic-tac-toe.webp";
+import Calculator from "../../asset/image/projects/basic-calculator.webp";
+import PianoTiles from "../../asset/image/projects/piano-tiles.webp";
+import PersonalPortfolio from "../../asset/image/projects/portfolio.webp";
+import FakeECommerceApp from "../../asset/image/projects/fake-e-commerce-web-app.webp";
+import ReactTypingEffect from "react-typing-effect";
 
 export class ProjectsList extends React.Component {
     public state = {
         projects: [
-            { id: 1, title: 'UnknownRori-PHP', link: "https://github.com/UnknownRori/UnknownRori-PHP", image: UnknownRoriPHP, description: "A custom non production web application framework inspired by Laravel" },
-            { id: 1, title: 'UnknownRori-PHP', link: "https://github.com/UnknownRori/UnknownRori-PHP", image: UnknownRoriPHP, description: "A custom non production web application framework inspired by Laravel" },
-            { id: 1, title: 'UnknownRori-PHP', link: "https://github.com/UnknownRori/UnknownRori-PHP", image: UnknownRoriPHP, description: "A custom non production web application framework inspired by Laravel" },
-            { id: 1, title: 'UnknownRori-PHP', link: "https://github.com/UnknownRori/UnknownRori-PHP", image: UnknownRoriPHP, description: "A custom non production web application framework inspired by Laravel" },
-            { id: 1, title: 'UnknownRori-PHP', link: "https://github.com/UnknownRori/UnknownRori-PHP", image: UnknownRoriPHP, description: "A custom non production web application framework inspired by Laravel" },
-            { id: 1, title: 'UnknownRori-PHP', link: "https://github.com/UnknownRori/UnknownRori-PHP", image: UnknownRoriPHP, description: "A custom non production web application framework inspired by Laravel" },
+            { id: 1, title: 'UnknownRori-PHP', link: "https://github.com/UnknownRori/UnknownRori-PHP", image: UnknownRoriPHP, description: "A custom non production web application framework inspired by Laravel." },
+            { id: 2, title: 'Simple Tic Tac Toe', link: "https://unknownrori.github.io/simple-tic-tac-toe/", image: TicTacToe, description: "A simple tic tac toe multiplayer written in vanilla javascript." },
+            { id: 3, title: 'Simple Calculator', link: "https://unknownrori.github.io/basic-online-calculator/", image: Calculator, description: "A simple calculator written in vanilla javascript." },
+            { id: 4, title: 'Piano Tiles', link: "https://unknownrori.github.io/piano-tiles/", image: PianoTiles, description: "A rhythmic piano game written in typescript" },
+            { id: 5, title: 'Personal Portfolio', link: "https://unknownrori-firebase.web.app/", image: PersonalPortfolio, description: "That's the page you viewing right now" },
+            { id: 6, title: 'Fake E-Commerce', link: "https://github.com/UnknownRori/fake-e-commerce-laravel-app", image: FakeECommerceApp, description: "A simple commerce web app." },
         ] as projectItem[],
     };
 
@@ -23,12 +29,19 @@ export class ProjectsList extends React.Component {
 
         return (
             <div className="flex flex-col">
+                <ReactTypingEffect text={["Projects", "Check out my github to see more!"]} className="mt-20 md:text-2xl lg:text-3xl sm:text-lg text-center" displayTextRenderer={(text: string, i: number) => {
+                    return (
+                        <p className={i == 0 ? 'profile-name' : ''}>
+                            {text}
+                        </p>
+                    );
+                }} />
 
                 <div className="flex flex-row flex-wrap justify-center">
                     {projectsList}
                 </div>
 
-                <div className="group m-auto text-center p-2 bg-gray-700 bg-opacity-80 rounded-md hover:bg-gray-900 hover:bg-opacity-95 duration-1000">
+                <div className="my-10 group m-auto text-center p-2 bg-gray-700 bg-opacity-80 rounded-md hover:bg-gray-900 hover:bg-opacity-95 duration-1000">
                     <a className="flex flex-row justify-center"
                         href="https://github.com/UnknownRori" data-target="_blank">
                         <span>
