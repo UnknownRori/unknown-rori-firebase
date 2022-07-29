3a1c41b3700a791b6cedc08e388e208f923212c6
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-07-30 06:09:14 +0700

    🔨 Added title attribute to Project Image

--- src/components/projects/ProjectItem.tsx
+++ src/components/projects/ProjectItem.tsx
@@ -4,8 +4,10 @@ import { projectItem } from "../../interface/ProjectItem";
 export class ProjectItem extends React.Component<{ project: projectItem }> {
     render(): ReactNode {
         return (
-            <div className="group md:w-3/12 sm:w-6/12 m-5 p-5 bg-gray-700 bg-opacity-60 rounded-md shadow-2xl ring-1 ring-gray-400 duration-1000
-                           hover:z-20 hover:-translate-y-5 hover:bg-gray-800 hover:bg-opacity-95 hover:ring-gray-600 hover:shadow-md"
+            <div title={this.props.project.description} className="group md:w-3/12 sm:w-6/12 m-5 p-5 
+            bg-gray-700 bg-opacity-60 rounded-md shadow-2xl ring-1 ring-gray-400 duration-1000
+            hover:z-20 hover:-translate-y-5 hover:bg-gray-800 hover:bg-opacity-95 hover:ring-gray-600 
+            hover:shadow-md"
             >
                 <p className="fixed z-50 bg-gray-800 bg-opacity-80 rounded opacity-0 shadow-2xl w-[90%] group-hover:opacity-100 duration-1000">
                     {this.props.project.description}
