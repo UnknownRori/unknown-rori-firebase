678f7c3b95ebb8af60a4a88c9b59368563cb5594
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-02 06:08:07 +0700

    🔨 Refactor and update tic tac toe

--- src/components/projects/ProjectsList.tsx
+++ src/components/projects/ProjectsList.tsx
@@ -1,24 +1,11 @@
 import React from "react";
-import { projectItem } from "../../interface/ProjectItem";
 import { ProjectItem } from "./ProjectItem";
-import UnknownRoriPHP from "../../asset/image/projects/php-framework.webp";
-import TicTacToe from "../../asset/image/projects/basic-hotseat-tic-tac-toe.webp";
-import Calculator from "../../asset/image/projects/basic-calculator.webp";
-import PianoTiles from "../../asset/image/projects/piano-tiles.webp";
-import PersonalPortfolio from "../../asset/image/projects/portfolio.webp";
-import FakeECommerceApp from "../../asset/image/projects/fake-e-commerce-web-app.webp";
 import ReactTypingEffect from "react-typing-effect";
+import ProjectList from "../../data/ProjectList";
 
 export class ProjectsList extends React.Component {
     public state = {
-        projects: [
-            { id: 1, title: 'UnknownRori-PHP', link: "https://github.com/UnknownRori/UnknownRori-PHP", image: UnknownRoriPHP, description: "A custom non production web application framework inspired by Laravel." },
-            { id: 2, title: 'Simple Tic Tac Toe', link: "https://unknownrori.github.io/simple-tic-tac-toe/", image: TicTacToe, description: "A simple tic tac toe multiplayer written in vanilla javascript." },
-            { id: 3, title: 'Simple Calculator', link: "https://unknownrori.github.io/basic-online-calculator/", image: Calculator, description: "A simple calculator written in vanilla javascript." },
-            { id: 4, title: 'Piano Tiles', link: "https://unknownrori.github.io/piano-tiles/", image: PianoTiles, description: "A rhythmic piano game written in typescript" },
-            { id: 5, title: 'Personal Portfolio', link: "https://unknownrori-firebase.web.app/", image: PersonalPortfolio, description: "That's the page you viewing right now" },
-            { id: 6, title: 'Fake E-Commerce', link: "https://github.com/UnknownRori/fake-e-commerce-laravel-app", image: FakeECommerceApp, description: "A simple commerce web app." },
-        ] as projectItem[],
+        projects: ProjectList
     };
 
     render(): React.ReactNode {
---
+++ src/data/ProjectList.ts
@@ -0,0 +1,17 @@
+import { projectItem } from "../interface/ProjectItem";
+
+import UnknownRoriPHP from "../asset/image/projects/php-framework.webp";
+import TicTacToe from "../asset/image/projects/tic-tac-toe.webp";
+import Calculator from "../asset/image/projects/basic-calculator.webp";
+import PianoTiles from "../asset/image/projects/piano-tiles.webp";
+import PersonalPortfolio from "../asset/image/projects/portfolio.webp";
+import FakeECommerceApp from "../asset/image/projects/fake-e-commerce-web-app.webp";
+
+export default [
+    { id: 1, title: 'UnknownRori-PHP', link: "https://github.com/UnknownRori/UnknownRori-PHP", image: UnknownRoriPHP, description: "A custom non production web application framework inspired by Laravel." },
+    { id: 2, title: 'Tic Tac Toe', link: "https://unknownrori.github.io/tic-tac-toe/", image: TicTacToe, description: "A customizable size, victory condition and shipped with my very first AI tic tac toe written using Typescript." },
+    { id: 3, title: 'Simple Calculator', link: "https://unknownrori.github.io/basic-online-calculator/", image: Calculator, description: "A simple calculator written in vanilla Javascript." },
+    { id: 4, title: 'Piano Tiles', link: "https://unknownrori.github.io/piano-tiles/", image: PianoTiles, description: "A rhythmic piano game written in Typescript" },
+    { id: 5, title: 'Personal Portfolio', link: "https://unknownrori-firebase.web.app/", image: PersonalPortfolio, description: "That's the page you viewing right now" },
+    { id: 6, title: 'Fake E-Commerce', link: "https://github.com/UnknownRori/fake-e-commerce-laravel-app", image: FakeECommerceApp, description: "A simple commerce web app." },
+] as projectItem[];
