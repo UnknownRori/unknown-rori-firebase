4c9fb16de270fa23ff5216e9ecd6f2c2e2dbfd5a
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-02 06:08:45 +0700

    🔨 Minor style change

--- src/components/projects/ProjectItem.tsx
+++ src/components/projects/ProjectItem.tsx
@@ -4,12 +4,14 @@ import { projectItem } from "../../interface/ProjectItem";
 export class ProjectItem extends React.Component<{ project: projectItem }> {
     render(): ReactNode {
         return (
-            <div title={this.props.project.description} className="group md:w-3/12 sm:w-6/12 m-5 p-5 
-            bg-gray-700 bg-opacity-60 rounded-md shadow-2xl ring-1 ring-gray-400 duration-1000
+            <div title={this.props.project.description} className="group lg:w-3/12 md:w-6/12 sm:w-6/12 
+            m-5 p-5 bg-gray-700 bg-opacity-60 rounded-md shadow-2xl ring-1 ring-gray-400 duration-1000
             hover:z-20 hover:-translate-y-5 hover:bg-gray-800 hover:bg-opacity-95 hover:ring-gray-600 
             hover:shadow-md"
             >
-                <p className="fixed pointer-events-none bg-gray-800 bg-opacity-80 rounded opacity-0 shadow-2xl w-[90%] group-hover:opacity-100 duration-1000">
+                <p className="fixed pointer-events-none p-2 sm:text-sm md:text-md lg:text-md
+                bg-gray-800 bg-opacity-80 rounded opacity-0 shadow-2xl w-[88%] 
+                group-hover:opacity-100 duration-1000">
                     {this.props.project.description}
                 </p>
                 <a href={this.props.project.link} target="_blank">
