a516050128b4de4add8315a7cbd461509892ab2e
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-06 06:32:11 +0700

    🔨 Extract background data

--- src/components/animation/Background.tsx
+++ src/components/animation/Background.tsx
@@ -1,15 +1,6 @@
 import useBackgroundAnimation from '../../hooks/useBackgroundAnimation';
-import background1 from './../../asset/image/background/background1.webp';
-import background2 from './../../asset/image/background/background2.webp';
-import background3 from './../../asset/image/background/background3.webp';
-import background4 from './../../asset/image/background/background4.webp';
-import background5 from './../../asset/image/background/background5.webp';
-import background6 from './../../asset/image/background/background6.webp';
 import BackgroundItem from './BackgroundItem';
-
-const BackgroundImages = [
-    background1, background2, background3, background4, background5, background6
-];
+import BackgroundImages from '../../data/Background';
 
 const Background = () => {
     const [interval] = useBackgroundAnimation(BackgroundImages);
---
+++ src/data/Background.ts
@@ -0,0 +1,12 @@
+import background1 from './../../asset/image/background/background1.webp';
+import background2 from './../../asset/image/background/background2.webp';
+import background3 from './../../asset/image/background/background3.webp';
+import background4 from './../../asset/image/background/background4.webp';
+import background5 from './../../asset/image/background/background5.webp';
+import background6 from './../../asset/image/background/background6.webp';
+
+const BackgroundImages = [
+    background1, background2, background3, background4, background5, background6
+];
+
+export default BackgroundImages;
