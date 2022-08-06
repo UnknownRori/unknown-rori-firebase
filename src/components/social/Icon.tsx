2c2e73ed2584cdd395c857eb3a96e5f7957e8fd2
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-07 04:27:09 +0700

    🔨 change from class component to function component

--- src/components/social/Icon.tsx
+++ src/components/social/Icon.tsx
@@ -1,13 +1,10 @@
-import React from "react";
 import IconProps from "../../types/Icon";
 
-export default class Icon extends React.Component<{ icon: IconProps }> {
-    render(): React.ReactNode {
-        return (
-            <a target='_blank' href={this.props.icon.href} className='m-2'>
-                <img className={"duration-1000 transition-all " + this.props.icon.className}
-                    src={this.props.icon.img} alt={this.props.icon.alt} />
-            </a>
-        );
-    }
+export default function Icon(props: { icon: IconProps }) {
+    return (
+        <a target='_blank' href={props.icon.href} className='m-2'>
+            <img className={"duration-1000 transition-all " + props.icon.className}
+                src={props.icon.img} alt={props.icon.alt} />
+        </a>
+    );
 }
