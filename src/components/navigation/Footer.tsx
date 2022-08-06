1b57baa362cba32e50dbaa49aa167d2b760cb620
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-07 04:16:04 +0700

    🔨 change from class component to function component

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
-
-export default class Footer extends React.Component {
-    render(): React.ReactNode {
-        const date = new Date();
-        return (
-            <footer className='w-screen fixed bottom-0 bg-gray-200 bg-opacity-20'>
-                <div className='wrapper flex flex-row m-auto items-center'>
-                    <div className='m-auto text-center'>
-                        <span>{date.getFullYear()}&copy; UnknownRori</span>
-                    </div>
+export default function Footer() {
+    const date = new Date();
+    return (
+        <footer className='w-screen fixed bottom-0 bg-gray-200 bg-opacity-20'>
+            <div className='wrapper flex flex-row m-auto items-center'>
+                <div className='m-auto text-center'>
+                    <span>{date.getFullYear()}&copy; UnknownRori</span>
                 </div>
-            </footer>
-        );
-    }
+            </div>
+        </footer>
+    );
 }
