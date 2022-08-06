690853ae3082a6f35ad34b4ac09df09cfcea2b96
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-07 04:27:22 +0700

    🔨 Refactor and change to function component

--- src/components/social/SocialList.tsx
+++ src/components/social/SocialList.tsx
@@ -1,22 +1,15 @@
-import React from "react";
 import Icon from './Icon';
 import SocialIconList from "../../data/SocialIconList";
 
-export class SocialList extends React.Component {
-    public state = {
-        icons: SocialIconList
-    };
+export default function SocialList() {
+    const socialIconList = SocialIconList.map((icon) =>
+        <Icon key={icon.id} icon={icon} />
+    );
 
-    render(): React.ReactNode {
-        const socialIconList = this.state.icons.map((icon) =>
-            <Icon key={icon.id} icon={icon} />
-        );
-
-        return (
-            <>
-                <h2 className="text-center">Link</h2>
-                {socialIconList}
-            </>
-        );
-    }
+    return (
+        <>
+            <h2 className="text-center">Link</h2>
+            {socialIconList}
+        </>
+    );
 }
