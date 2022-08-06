a9dd6abe8f00a4b086f2c38b432bfb04408d69af
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-07 04:20:08 +0700

    🔨 Change from class component to function component

--- src/components/profile/ProfileImage.tsx
+++ src/components/profile/ProfileImage.tsx
@@ -1,14 +1,11 @@
-import React from "react";
 import LazyLoadImage from "../animation/LazyLoadImage";
 import profile from './../../asset/image/UnknownRori.webp';
 
-export default class ProfileImage extends React.Component {
-    render(): React.ReactNode {
-        return (
-            <figure className="m-auto rounded-full p-3 shadow-lg">
-                <LazyLoadImage className="h-56 w-56 object-cover rounded-full border-2 border-gray-300"
-                    src={profile} alt="UnknownRori" />
-            </figure>
-        );
-    }
+export default function ProfileImage() {
+    return (
+        <figure className="m-auto rounded-full p-3 shadow-lg">
+            <LazyLoadImage className="h-56 w-56 object-cover rounded-full border-2 border-gray-300"
+                src={profile} alt="UnknownRori" />
+        </figure>
+    );
 }
