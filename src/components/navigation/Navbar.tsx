d4b5163e866a8a9e19961ee8bcccbb660efed0a8
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-07 04:16:26 +0700

    🔨 change from class component to function component

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,45 +1,34 @@
-import React from "react";
 import NavbarProps from "../../types/NavbarProps";
 
-export default class Navbar extends React.Component<NavbarProps> {
+export default function Navbar(props: NavbarProps) {
 
-    constructor(props: NavbarProps) {
-        super(props);
+    const Profile = () => {
+        props.changePage(0);
+    };
 
-        this.Profile = this.Profile.bind(this);
-        this.Projects = this.Projects.bind(this);
-        this.AboutMe = this.AboutMe.bind(this);
-    }
+    const Projects = () => {
+        props.changePage(1);
+    };
 
-    Profile() {
-        this.props.changePage(0);
-    }
+    const AboutMe = () => {
+        props.changePage(2);
+    };
 
-    Projects() {
-        this.props.changePage(1);
-    }
-
-    AboutMe() {
-        this.props.changePage(2);
-    }
-
-    render(): React.ReactNode {
-        return (
-            <nav className="bg-gray-200 bg-opacity-20 fixed w-screen p-1 z-20">
-                <div className="wrapper">
-                    <ul className="flex flex-row-reverse">
-                        <li className="nav-item">
-                            <button className="nav-link" onClick={this.Profile}>Profile</button>
-                        </li>
-                        <li className="nav-item">
-                            <button className="nav-link" onClick={this.Projects}>Projects</button>
-                        </li>
-                        <li className="nav-item">
-                            <button className="nav-link" onClick={this.AboutMe}>About me</button>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        );
-    }
+    return (
+        <nav className="bg-gray-200 bg-opacity-20 fixed w-screen p-1 z-20">
+            <div className="wrapper">
+                <ul className="flex flex-row-reverse">
+                    <li className="nav-item">
+                        <button className="nav-link" onClick={Profile}>Profile</button>
+                    </li>
+                    <li className="nav-item">
+                        <button className="nav-link" onClick={Projects}>Projects</button>
+                    </li>
+                    <li className="nav-item">
+                        <button className="nav-link" onClick={AboutMe}>About me</button>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
