11ea51261aa283942687e7ee3ab4cd1aa51ad009
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-07 04:20:03 +0700

    🔨 Change from class component to function component

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import ProfileImage from "./ProfileImage";
 import ProfileIntro from "./ProfileIntro";
 
-export default class Profile extends React.Component {
-    render(): React.ReactNode {
-        return (
-            <header id="profile" className="h-screen flex">
-                <div className="flex flex-col self-center w-[100%]">
-                    <ProfileImage />
-                    <ProfileIntro />
-                </div>
-            </header>
-        );
-    }
+export default function Profile() {
+    return (
+        <header id="profile" className="h-screen flex">
+            <div className="flex flex-col self-center w-[100%]">
+                <ProfileImage />
+                <ProfileIntro />
+            </div>
+        </header>
+    );
 }
