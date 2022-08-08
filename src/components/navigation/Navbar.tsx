f18350d59106b5ab33e5488ae6c4cb7144c0d1a4
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-08 14:43:18 +0700

    🔨 Implement navigation using custom link

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,23 +1,19 @@
 import NavbarProps from "../../types/NavbarProps";
+import Link from "./Link";
 
 export default function Navbar(props: NavbarProps) {
-
-    const changePage = (pageName: string) => {
-        props.changePage(pageName);
-    };
-
     return (
         <nav className="bg-gray-200 bg-opacity-20 fixed w-screen p-1 z-20">
             <div className="wrapper">
                 <ul className="flex flex-row-reverse">
                     <li className="nav-item">
-                        <button className="nav-link" onClick={() => changePage('home')}>Profile</button>
+                        <Link className='nav-link' href='/' changePage={props.changePage}>Profile</Link>
                     </li>
                     <li className="nav-item">
-                        <button className="nav-link" onClick={() => changePage('projects')}>Projects</button>
+                        <Link className='nav-link' href='/projects' changePage={props.changePage}>Projects</Link>
                     </li>
                     <li className="nav-item">
-                        <button className="nav-link" onClick={() => changePage('aboutme')}>About me</button>
+                        <Link className='nav-link' href='/about-me' changePage={props.changePage}>About me</Link>
                     </li>
                 </ul>
             </div>
