292060063d9bb9333d353b6812fcf773b1806650
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-08 14:52:25 +0700

    🔨 Properly implement 404 page

--- src/page/404.tsx
+++ src/page/404.tsx
@@ -5,6 +5,11 @@ export default function NotFound(props: PageProps) {
     const animate = usePageAnimation(props);
 
     return (
-        <div className={"wrapper " + animate}>404</div>
+        <div className={"wrapper " + animate}>
+            <div className="flex justify-center flex-col">
+                <h1 className="text-center text-6xl">404</h1>
+                <p className="text-center text-2xl">Page not found</p>
+            </div>
+        </div>
     );
 }
