d579b6065100ea769668fc160dbd22a176874b1e
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-10 04:05:10 +0700

    🔨 Convert from class component to function component

--- src/components/projects/ProjectsList.tsx
+++ src/components/projects/ProjectsList.tsx
@@ -1,46 +1,38 @@
-import React from "react";
 import ProjectItem from "./ProjectItem";
 import ReactTypingEffect from "react-typing-effect";
-import ProjectList from "../../data/ProjectList";
+import ProjectListData from "../../data/ProjectList";
 
-export default class ProjectsList extends React.Component {
-    public state = {
-        projects: ProjectList
-    };
+export default function ProjectList() {
+    const projectListElement: JSX.Element[] = ProjectListData.map((project) =>
+        <ProjectItem project={project} key={project.id} />
+    );
 
-    render(): React.ReactNode {
+    return (
+        <div className="flex flex-col">
+            <ReactTypingEffect text={["Projects", "Check out my github to see more!"]} className="mt-20 md:text-2xl lg:text-3xl sm:text-lg text-center" displayTextRenderer={(text: string, i: number) => {
+                return (
+                    <p className={i == 0 ? 'profile-name' : ''}>
+                        {text}
+                    </p>
+                );
+            }} />
 
-        const projectsList: JSX.Element[] = this.state.projects.map((project) =>
-            <ProjectItem project={project} key={project.id} />
-        );
-
-        return (
-            <div className="flex flex-col">
-                <ReactTypingEffect text={["Projects", "Check out my github to see more!"]} className="mt-20 md:text-2xl lg:text-3xl sm:text-lg text-center" displayTextRenderer={(text: string, i: number) => {
-                    return (
-                        <p className={i == 0 ? 'profile-name' : ''}>
-                            {text}
-                        </p>
-                    );
-                }} />
-
-                <div className="flex flex-row flex-wrap justify-center">
-                    {projectsList}
-                </div>
-
-                <div className="my-10 group m-auto text-center p-2 bg-gray-700 bg-opacity-80 rounded-md hover:bg-gray-900 hover:bg-opacity-95 duration-1000">
-                    <a className="flex flex-row justify-center"
-                        href="https://github.com/UnknownRori" data-target="_blank">
-                        <span>
-                            Show All
-                        </span>
-                        <svg className="group-hover:translate-x-1 duration-1000 self-center" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd"
-                                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
-                        </svg>
-                    </a>
-                </div>
+            <div className="flex flex-row flex-wrap justify-center">
+                {projectListElement}
             </div>
-        );
-    }
+
+            <div className="my-10 group m-auto text-center p-2 bg-gray-700 bg-opacity-80 rounded-md hover:bg-gray-900 hover:bg-opacity-95 duration-1000">
+                <a className="flex flex-row justify-center"
+                    href="https://github.com/UnknownRori" data-target="_blank">
+                    <span>
+                        Show All
+                    </span>
+                    <svg className="group-hover:translate-x-1 duration-1000 self-center" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                    </svg>
+                </a>
+            </div>
+        </div>
+    );
 }
