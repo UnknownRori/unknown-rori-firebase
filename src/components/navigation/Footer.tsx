55a0aa139ee120e6dfb8d47ec5a53abca38d67e5
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-26 05:35:44 +0700

    🔨 Change footer message

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className='w-screen fixed bottom-0 bg-gray-200 bg-opacity-20'>
             <div className='wrapper flex flex-row m-auto items-center'>
                 <div className='m-auto text-center'>
-                    <span>{date.getFullYear()}&copy; UnknownRori</span>
+                    <span>{date.getFullYear()}&copy; Made with ❤️ UnknownRori</span>
                 </div>
             </div>
         </footer>
