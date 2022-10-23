306adff085f6237edf7f1e18b3a707704b722683
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-10-23 17:14:40 +0700

    📦 Implement animation on hover for nav link

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,19 +1,20 @@
 import Link from "./Link";
+import LinkProps from '../../types/LinkProps';
 
+const NavLinkProps = [
+    { href: '/', children: 'Profile' },
+    { href: '/projects', children: 'Projects' },
+    { href: '/about-me', children: 'About Me' },
+] as LinkProps[];
 export default function Navbar() {
     return (
         <nav className="bg-gray-200 bg-opacity-20 fixed w-screen p-1 z-20">
             <div className="wrapper">
                 <ul className="flex flex-row-reverse">
-                    <li className="nav-item">
-                        <Link className='nav-link' href='/'>Profile</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className='nav-link' href='/projects'>Projects</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className='nav-link' href='/about-me'>About me</Link>
-                    </li>
+                    {NavLinkProps.map((val, index) => {
+                        const className = window.location.pathname == val.href ? 'nav-link active' : 'nav-link';
+                        return (<Link key={index} className={className} href={val.href}>{val.children}</Link>);
+                    })}
                 </ul>
             </div>
         </nav>
