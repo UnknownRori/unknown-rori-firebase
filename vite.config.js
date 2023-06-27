1fe6f20301564c874cfe264b15c2e8bb1b937107
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-06-27 14:05:30 +0700

    📦 Added alias and fix indentation

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { URL, fileURLToPath } from "url";
 
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        }
+    }
 });
