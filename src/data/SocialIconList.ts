d53934e9451140eaf549eb6a0c75ada87d2456be
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-06-29 05:22:26 +0700

    🛠️ Fix `href` on social list

--- src/data/SocialIconList.ts
+++ src/data/SocialIconList.ts
@@ -9,5 +9,5 @@ export default [
     { id: 1, className: "rounded-lg w-10 md:w-12 hover:translate-x-3", href: "https://github.com/UnknownRori", img: github, alt: "Github" },
     { id: 2, className: "rounded-lg w-10 md:w-12 hover:translate-x-3", href: "https://stackoverflow.com/users/13991588/unknownrori", img: stackoverflow, alt: "Stackoverflow" },
     { id: 3, className: "rounded-lg w-10 md:w-12 hover:translate-x-3", href: "https://twitter.com/UnknownRori", img: twitter, alt: "Twitter" },
-    { id: 4, className: "rounded-lg w-10 md:w-12 hover:translate-x-3", href: "https://twitter.com/UnknownRori", img: linkedin, alt: "Linkedin" },
+    { id: 4, className: "rounded-lg w-10 md:w-12 hover:translate-x-3", href: "https://www.linkedin.com/in/unknownrori", img: linkedin, alt: "Linkedin" },
 ] as IconProps[];
