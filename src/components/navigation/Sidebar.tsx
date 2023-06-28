508edab7120fa13edb4d6c1b189671c30a3ae834
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-06-28 14:01:06 +0700

    📦 Added linkedin link

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -3,11 +3,11 @@ import SocialList from "../social/SocialList";
 
 export default function Sidebar() {
     return (
-        <div className="top-44 fixed z-10">
+        <div className="top-24 fixed z-10">
             <button className="button-secondary fixed m-3 -translate-x-8 w-12 transition-all duration-1000 group peer focus:translate-x-14 z-10">
                 <img src={arrow} alt="=>" className="group-focus:rotate-0 rotate-180 transition-all duration-1000" />
             </button>
-            <div className="sidebar peer-focus:-translate-x-2 top-44 fixed
+            <div className="sidebar peer-focus:-translate-x-2 top-24 fixed
                 bg-gray-900 bg-opacity-60 rounded-md p-6 transition-all duration-1000 -translate-x-[86px]">
                 <SocialList />
             </div>
