c1ef1ea2eaa74b1b8f5df00e6a2d5cedfe7ee167
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-06-29 04:57:03 +0700

    🛠️ Added new line

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -8,8 +8,13 @@ export default function Sidebar() {
 
     return (
         <div className="top-24 fixed z-10">
-            <button onClick={toggleState} className={`button-secondary fixed m-3 ${state ? 'translate-x-14' : '-translate-x-8'} w-12 transition-all duration-1000 z-10`}>
-                <img src={arrow} alt="=>" className={`${state ? 'rotate-0' : 'rotate-180'} transition-all duration-1000`} />
+            <button onClick={toggleState}
+                className={`button-secondary fixed m-3 ${state ? 'translate-x-14' : '-translate-x-8'} 
+                w-12 transition-all duration-1000 z-10`}
+            >
+                <img src={arrow} alt="=>"
+                    className={`${state ? 'rotate-0' : 'rotate-180'} transition-all duration-1000`}
+                />
             </button>
             <div className={`sidebar ${state ? '-translate-x-2' : '-translate-x-[86px]'} top-24 fixed
                 bg-gray-900 bg-opacity-60 rounded-md p-6 transition-all duration-1000`}>
