58cdaa8fac4c40c08f499c9d788ec84c4125bbb4
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-08-21 02:48:34 +0700

    📦 Added vite pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { URL, fileURLToPath } from "url";
+import { VitePWA } from "vite-plugin-pwa";
+
+import PWAOptions from './src/vitePWA';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), VitePWA(PWAOptions)],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
