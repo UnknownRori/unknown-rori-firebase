3e281985fea699ba65e8940259396c868db3d32a
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-08-28 06:16:54 +0700

    📦 Change footer color

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     const date = new Date();
 
     return (
-        <footer className='w-screen fixed bottom-0 bg-gray-200 bg-opacity-20'>
+        <footer className='w-screen fixed bottom-0 bg-gray-800 bg-opacity-60'>
             <div className='wrapper flex flex-row m-auto items-center'>
                 <div className='m-auto text-center'>
                     <span>{date.getFullYear()}&copy; Made with ❤️ by UnknownRori</span>
